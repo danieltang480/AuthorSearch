37b0efca4d492da9461d98941667d766a2d781b9
danieltang480 <133431729+danieltang480@users.noreply.github.com>
2023-06-25 22:05:43 -0400

    Update website.py

--- website.py
+++ website.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request
 import requests
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,16 +9,16 @@ def home():
 
 @app.route("/search", methods=["POST"])
 def search():
-    author = request.form.get("author")  
+    author = request.form.get("author")
 
     source = 'https://www.googleapis.com/books/v1/volumes'
     params = {
         'q': f'inauthor:"{author}"',
         'maxResults': 40,
-        'langRestrict': 'english'  
+        'langRestrict': 'en'
     }
 
-    books = set()  
+    books = set()
     results = []
 
     while True:
@@ -35,11 +36,22 @@ def search():
                     language = volume_info.get('language', '')
 
                     if language.lower() == 'en':
+                        book_id = item.get('id', '')
+                        ratings = get_book_ratings(book_id)
+                        if ratings:
+                            average_rating = ratings['averageRating']
+                            ratings_count = ratings['ratingsCount']
+                        else:
+                            average_rating = 'N/A'
+                            ratings_count = 0
+
                         book_info = {
                             'title': title,
                             'authors': authors,
                             'publisher': publisher,
-                            'description': description
+                            'description': description,
+                            'average_rating': average_rating,
+                            'ratings_count': ratings_count
                         }
                         results.append(book_info)
                         books.add(title)
@@ -50,6 +62,18 @@ def search():
             break
 
     return render_template("search.html", author=author, results=results)
+
+def get_book_ratings(book_id):
+    url = f"https://www.googleapis.com/books/v1/volumes/{book_id}"
+    response = requests.get(url)
+    data = response.json()
+
+    if "averageRating" in data["volumeInfo"]:
+        average_rating = data["volumeInfo"]["averageRating"]
+        ratings_count = data["volumeInfo"]["ratingsCount"]
+        return {'averageRating': average_rating, 'ratingsCount': ratings_count}
+    else:
+        return None
 
 if __name__ == "__main__":
     app.run()
